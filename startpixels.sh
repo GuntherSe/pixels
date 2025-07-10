bc014ee741b1d34cf358f8d3e952ec1083b724a1
GuntherSe <gunther.seiser.63@gmail.com>
2025-07-10 20:56:36 +0200

    update startscripts

--- startpixels.sh
+++ startpixels.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # start pixels
 
+# OLA reload (ArtNet might not be available):
+curl http://127.0.0.1:9090/reload
+
 source /home/gunther/.venv/bin/activate
 
 cd ~/Dokumente/python/pixels
